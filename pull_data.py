bf19ff4d7d9b5076582a3e3e401757e37b6f7c93
Brian McConeghy <brianmcconeghy@gmail.com>
2025-01-06 16:06:34 -0800

    Make this a Typer CLI

--- pull_data.py
+++ pull_data.py
@@ -1,9 +1,12 @@
+import datetime
 import json
+import os
 import requests
 from enum import StrEnum
 
 import pandas as pd
 import structlog
+import typer
 from rich.pretty import pprint
 
 logger = structlog.get_logger()
@@ -16,12 +19,16 @@ SCRYFALL_USER_AGENT = "GM-APP"
 SCRYFALL_ACCEPT = "*/*"
 
 
+app = typer.Typer()
+
+
 class BulkDataType(StrEnum):
     oracle_cards = "oracle_cards"
     unique_artwork = "unique_artwork"
     default_cards = "default_cards"
     all_cards = "all_cards"
     rulings = "rulings"
+
 
 class Finish(StrEnum):
     foil = "foil"
@@ -33,13 +40,15 @@ def download_file(url: str, destination: str):
     with requests.get(url, stream=True) as response:
         # Check if the request was successful
         if response.status_code == 200:
-            with open(destination, 'wb') as file:
+            with open(destination, "wb") as file:
                 for chunk in response.iter_content(chunk_size=8192):
                     if chunk:
                         file.write(chunk)
             logger.info("Download completed", destination=destination)
         else:
-            logger.error(f"Failed to retrieve the file. HTTP Status: {response.status_code}")
+            logger.error(
+                f"Failed to retrieve the file. HTTP Status: {response.status_code}"
+            )
 
 
 def flatten_dict(d: dict, parent_key: str = "", sep: str = "_") -> dict:
@@ -54,60 +63,70 @@ def flatten_dict(d: dict, parent_key: str = "", sep: str = "_") -> dict:
 
 
 def find_cards(
-    card_name: str, set_name: str | None = None, finish: Finish | None = None, database: list[dict] | None = None
+    card_name: str,
+    set_name: str | None = None,
+    finish: Finish | None = None,
+    db: list[dict] | None = None,
 ) -> list:
-    if not database:
-
+    # TODO: Add in logic to allow db being passed in
+    # TODO: Add logic to parse out the set_name and finish
     card_name = card_name.lower()
-    set_name = set_name.lower()
+    all_cards_with_name = [card for card in db if card_name in card["name"].lower()]
 
-    all_cards_with_name = [card for card in BULK if card_name in card["name"].lower()]
+    return all_cards_with_name
 
 
 def get_database_uris():
-    db_uri_response = requests.get(f"{SCRYFALL_URL}/bulk-data", headers={"User-Agent": SCRYFALL_USER_AGENT, "Accept": SCRYFALL_ACCEPT})
+    db_uri_response = requests.get(
+        f"{SCRYFALL_URL}/bulk-data",
+        headers={"User-Agent": SCRYFALL_USER_AGENT, "Accept": SCRYFALL_ACCEPT},
+    )
     if db_uri_response.status_code == 200:
         return json.loads(db_uri_response.text)
 
 
-def download_default_cards(local_path: str, bulk_data_type: BulkDataType = BulkDataType.default_cards):
+@app.command()
+def download_default_cards(
+    # Incorrectly typed here because Typer doesn't support `| None`
+    local_path: str = None,
+    bulk_data_type: BulkDataType = BulkDataType.default_cards,
+) -> str:
+    if not local_path:
+        cwd = os.getcwd()
+        datetime_stamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
+        local_path = f"{cwd}/{datetime_stamp}_scryfall.json"
     all_dbs = get_database_uris()
     for db in all_dbs["data"]:
         if db["type"] == bulk_data_type:
             uri = db["download_uri"]
             logger.info("Downloading database", database=bulk_data_type, uri=uri)
             download_file(uri, local_path)
-
-with open(f"{BASE_DIR}/default-cards-20241231100753.json") as f:
-    BULK = json.load(f)
-
-card_records = [flatten_dict(card) for card in BULK]
-
-unique_set_names = pd.DataFrame(
-    sorted(set([cr["set_name"] for cr in BULK])), columns=["Set Name"]
-)
-unique_set_names.to_csv(f"{BASE_DIR}/set_names.csv", index=False)
-
-df = pd.DataFrame(card_records)
-
-df.to_csv(f"{BASE_DIR}/default-cards-20241231100753.csv", index=False)
-
-df_usd_only = df[
-    df[["prices_usd", "prices_usd_foil", "prices_usd_etched"]].notna().any(axis=1)
-]
-
-df_usd_only.to_csv(
-    f"{BASE_DIR}/default-cards-20241231100753_USD_only.csv", index=False
-)
+            return local_path
 
 
-# New bulk data for 2025-01-01
+def generate_dataframe_from_db(db_path: str) -> pd.DataFrame:
+    with open(db_path) as f:
+        bulk_data = json.load(f)
 
-with open(f"{BASE_DIR}/default-cards-20250101100710.json") as f:
-    NEWER_BULK = json.load(f)
+    card_records = [flatten_dict(card) for card in bulk_data]
 
-new_df = pd.DataFrame([flatten_dict(card) for card in NEWER_BULK])
-new_df_usd_only = df[
-    new_df[["prices_usd", "prices_usd_foil", "prices_usd_etched"]].notna().any(axis=1)
-]
-merged = pd.merge(df_usd_only, new_df_usd_only, on="id", suffixes=["_old", "_new"])
+    return pd.DataFrame(card_records)
+
+
+@app.command()
+def convert_json_db_to_csv(
+    output_path: str = typer.Option(
+        help="Path to where the CSV will be written on your local machine"
+    ),
+    json_db_path: str = typer.Option(
+        default=None, help="Path to the Scryfall DB on your local machine"
+    ),
+):
+    if not json_db_path:
+        db_path = download_default_cards()
+    df = generate_dataframe_from_db(db_path)
+    df.to_csv(output_path, index=False)
+
+
+if __name__ == "__main__":
+    app()
